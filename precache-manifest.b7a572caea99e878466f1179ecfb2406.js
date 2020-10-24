5555734cf2c34a626a7b0f347c03b4b1878e950a
Xt-wml <3526844055@qq.com>
2020-10-24 15:11:24 +0800

    Updates

---
+++ precache-manifest.b7a572caea99e878466f1179ecfb2406.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fcfb2b75fc868b4b1a19076f8841c2e",
+    "url": "/git@github.com:xt-wml/aliGW.git/index.html"
+  },
+  {
+    "revision": "8edf782a0441469480d1",
+    "url": "/git@github.com:xt-wml/aliGW.git/static/css/main.632a3239.chunk.css"
+  },
+  {
+    "revision": "416ba261c27992e407de",
+    "url": "/git@github.com:xt-wml/aliGW.git/static/js/2.35a500f7.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/git@github.com:xt-wml/aliGW.git/static/js/2.35a500f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8edf782a0441469480d1",
+    "url": "/git@github.com:xt-wml/aliGW.git/static/js/main.ed8ddb7b.chunk.js"
+  },
+  {
+    "revision": "6ea595121ef1bacacb78",
+    "url": "/git@github.com:xt-wml/aliGW.git/static/js/runtime-main.0de0d6bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git@github.com:xt-wml/aliGW.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
